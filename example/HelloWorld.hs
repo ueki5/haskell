d14b93f080ecd1c3832ce5334794b7d252aaa45e
unknown <Administrator@.(none)>
2010-03-25 20:15:26 +0900

    日本語が使えた！

--- example/HelloWorld.hs
+++ example/HelloWorld.hs
@@ -1,3 +1,23 @@
-﻿import System.IO.UTF8
-main = do 
-  System.IO.UTF8.putStrLn "こんにちは　世界!!"
+import System.IO
+main = do
+  hSetEncoding stdout utf8
+  hSetEncoding stdin utf8
+  putStrLn "こんにちは　世界!!"
+  str <- getLine
+  putStrLn (addMsg str)
+  otherMsg
+addMsg::String -> String
+addMsg msg = msg ++ "だそうです。"
+otherMsg = do
+  putStrLn "さようなら。"
+-- import System.IO
+-- main = do
+--   h <- openFile filename WriteMode
+--   hSetEncoding h utf8
+--   hSetEncoding stdout utf8
+--   hPutStrLn h "こんにちは　世界!!"
+--   putStrLn "こんにちは　世界!!"
+--   hClose h
+-- filename = "hello.txt"
+
+  
