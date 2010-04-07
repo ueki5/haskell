b3bdf149adeb6fc615eeafecbd032557acffec06
unknown <Administrator@.(none)>
2010-04-07 17:37:57 +0900

    練習問題 4.まで。 6.b.をやりかけ

--- study/chap11.hs
+++ study/chap11.hs
@@ -1,11 +1,15 @@
 data Op = Add | Sub | Mul | Div deriving Show
 valid :: Op -> Int -> Int -> Bool
--- valid Add _ _ = True
-valid Add x y = x >= y
+
+-- valid Add x y = x >= y
+-- valid Sub x y = x > y
+-- valid Mul x y = x /= 1 && y /= 1 && x >= y
+-- valid Div x y = y /= 1 && x `mod` y == 0
+valid Add _ _ = True
 valid Sub x y = x > y
--- valid Mul _ _ = True
-valid Mul x y = x /= 1 && y /= 1 && x >= y
-valid Div x y = y /= 1 && x `mod` y == 0
+valid Mul _ _ = True
+valid Div x y = x `mod` y == 0
+
 apply :: Op -> Int -> Int -> Int
 apply Add x y = x + y
 apply Sub x y = x - y
@@ -42,8 +46,6 @@ choices [] = [[]]
 -- choices xs = concat (map perms (subs xs))
 choices xs = [xs''| xs' <- subs xs,
                          xs'' <- perms xs']
--- isChoice :: [a] -> [a] -> Bool
--- isChoice' (x:xs ys) = 
 solution :: Expr -> [Int] -> Int -> Bool
 solution e xs n = elem (values e) (choices xs) && (eval e == [n])
 split :: [a] -> [([a],[a])]
@@ -52,6 +54,7 @@ split [_] = []
 split (x:xs) = ([x],xs):[(x:ls,rs)|(ls,rs) <- split xs]
 -- split xs = [(take n xs, drop n xs)|n <- [1 .. splitTo]]
 --     where splitTo = (length xs) - 1
+exprs :: [Int] -> [Expr]
 exprs [] = []
 exprs [n] = [Val n]
 exprs xs = [e | (ls, rs) <- split xs
@@ -80,6 +83,25 @@ solutions' :: [Int] -> Int -> [Expr]
 solutions' xs n = [e |xs' <- choices xs,
                       (e, n') <- results xs',
                       n' == n]
+isChoise' :: Ord a => a -> [a] -> [a]
+isChoise' x [] = []
+isChoise' x (y:ys) | x == y = ys
+                   | otherwise = y:(isChoise' x ys)
+isChoise :: Ord a => [a] -> [a] -> Bool
+isChoise xs (y:ys) = isChoise (isChoise' y xs) ys
+isChoise xs ys | xs == [] = True
+               | xs /= [] && ys == [] = False
+allCase :: [Int] -> [Expr]
+-- allCase xs = concat (map exprs (choices xs))
+allCase xs = [es | xs' <- choices xs,
+                   es    <- exprs xs']
+validCase :: [Int] -> [Expr]
+validCase xs = [es | es <- allCase xs, eval es /= []]
+-- data CheckVal = Infinate | Int
+-- solutions''' :: [Int] -> Int -> ChackVal -> ([Expr], [Expr])
+-- solutions''' xs n = [e |xs' <- choices xs,
+--                       (e, n') <- results xs',
+--                       n' == n]
 v1 = Val 1
 v2 = Val 2
 e1 = App Add v1 v2
