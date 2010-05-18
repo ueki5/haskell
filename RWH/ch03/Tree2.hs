28308a6df1b511a762adceed2f0ac14bcfde7784
ueki <ueki@LN500.(none)>
2010-05-19 01:34:53 +0900

    Tree型のMaybeバージョン

--- RWH/ch03/Tree2.hs
+++ RWH/ch03/Tree2.hs
@@ -1,13 +1,11 @@
 module Tree2 where
-data Tree2 a = Maybe a (Maybe (Tree2 a)) (Maybe (Tree2 a))
-             | Nobody
+data Tree2 a = Node a (Maybe (Tree2 a)) (Maybe (Tree2 a))
               deriving (Show,Eq,Ord)
--- t0 = Nobody
--- t1 = Just 1 t0 t0
--- t1 = Node 1 Empty Empty
--- t2 = Node 2 Empty Empty
--- t3 = Node 3 t1 t2
--- t4 = Node 4 t3 t1
+t0 = Node 0 Nothing Nothing
+t1 = Node 1 (Just t0) (Just t0)
+t2 = Node 2 (Just t0) (Just t1)
+t3 = Node 3 (Just t1) (Just t2)
+t4 = Node 4 (Just t2) (Just t3)
 -- left :: Tree2 a -> Tree2 a
 -- left Empty = Empty
 -- left (Node x l r) = l
