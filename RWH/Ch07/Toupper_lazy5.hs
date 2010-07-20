b716bf2a2960ab69caedce15a10a2b594e698422
unknown <Administrator@.(none)>
2010-07-20 12:56:23 +0900

    upd

--- RWH/Ch07/Toupper_lazy5.hs
+++ RWH/Ch07/Toupper_lazy5.hs
@@ -1,5 +1,7 @@
 module Ch07.Toupper_lazy5 where 
-import Data.Char(toUpper)
+import Data.Char(toUpper,isUpper,toLower)
 
 main :: IO ()
-main = interact ((++) "Your data, in uppercase, is:\n\n" . map toUpper)
+-- main = interact ((++) "Your data, in uppercase, is:\n\n" . map toUpper)
+main = interact ((++) "Your data, in uppercase, is:\n\n" . map reversal)
+    where reversal c = if isUpper c then toLower c else toUpper c
