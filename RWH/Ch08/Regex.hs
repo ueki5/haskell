814de7a8a7afdc70d3cb123eb024c181a381fb71
unknown <Administrator@.(none)>
2010-07-26 14:54:47 +0900

    Upd

--- RWH/Ch08/Regex.hs
+++ RWH/Ch08/Regex.hs
@@ -1,4 +1,7 @@
 import Text.Regex.Posix
 main = do
-  contents <- getLine
-  putStrLn contents
+  ret <- return ("your right hand" =~ "bar" :: Bool)
+  putStrLn $ show ret
+  ret <- return ("I,Buu. Ionsanii, uurit a lift'd batch" =~ "(uu|ii)" :: String)
+  putStrLn $ show ret
+  return ()
