322ba21e80be7ebd70f80704de9f73639441428b
ueki <ueki@LN500.(none)>
2010-08-17 00:27:13 +0900

    upd

--- RWH/Ch09/FoldDir.hs
+++ RWH/Ch09/FoldDir.hs
@@ -1,9 +1,42 @@
 module Ch09.FoldDir where
-import Ch09.ControlledVisit (Info)
+import Data.Char (toLower)
+import Ch09.ControlledVisit (Info(..),getUsefulContents,getInfo,isDirectory)
+import System.FilePath ((</>),takeFileName,takeExtension)
 data Iterate seed = Done { unwrap :: seed }
                   | Skip { unwrap :: seed }
                   | Continue { unwrap :: seed }
                     deriving (Show)
 type Iterator seed = seed -> Info -> Iterate seed
 foldTree :: Iterator a -> a -> FilePath -> IO a
-foldTree = undefined
+foldTree iter initSeed path = do
+    endSeed <- fold initSeed path
+    return (unwrap endSeed)
+  where 
+    fold seed subpath = getUsefulContents subpath >>= walk seed
+    walk seed (name:names) = do
+      let path' = path </> name
+      info <- getInfo path'
+      case iter seed info of
+        done@(Done _) -> return done
+        Skip seed' -> walk seed' names
+        Continue seed'
+          | isDirectory info -> do
+              next <- fold seed' path'
+              case next of
+                done@(Done _) -> return done
+                seed'' -> walk (unwrap seed'') names
+          | otherwise -> walk seed' names
+    walk seed _ = return (Continue seed)
+
+atMostThreePictures :: Iterator [FilePath]
+atMostThreePictures paths info
+    | length paths == 3
+      = Done paths
+    | isDirectory info && takeFileName path == ".svn"
+      = Skip paths
+    | extension `elem` [".jpg",".png"] 
+      = Continue (path:paths)
+    | otherwise
+      = Continue paths
+  where extension = map toLower (takeExtension path)
+        path = infoPath info
