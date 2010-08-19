eb76a881b619191c8ef873343ec528c002d51942
unknown <Administrator@.(none)>
2010-08-19 13:22:08 +0900

    upd

--- RWH/Ch10/Parse.hs
+++ RWH/Ch10/Parse.hs
@@ -1,10 +1,14 @@
 module Ch10.Parse where
+import qualified Data.ByteString.Lazy.Char8 as L8
 import qualified Data.ByteString.Lazy as L
+import Data.Word
+
 import Data.Int(Int64)
+-- import Ch10.PNM
 data ParseState = ParseState {
       string::L.ByteString 
      ,offset::Int64
-    }
+    } deriving (Show)
 simpleParse :: ParseState -> (a, ParseState)
 simpleParse = undefined
 betterParse :: ParseState -> (a, ParseState)
@@ -16,9 +20,41 @@ newtype Parse a = Parse {
 identity :: a -> Parse a
 identity a = Parse (\s -> Right (a, s))
 
--- parse :: Parse a -> ParseState -> Either String (a, ParseState)
-parse :: Parse a -> L.ByteString -> Either String a
+-- parse :: Parse a -> .ByteString -> Either String a
+-- parse parser initState 
+--     = case runParse parser (ParseState initState 0) of
+--         Left err -> Left err
+--         Right (a, _) -> Right a
+parse :: Parse a -> ParseState -> Either String (a, ParseState)
 parse parser initState 
-    = case runParse parser (ParseState initState 0) of
-        Left err -> Left err
-        Right (a, _) -> Right a
+    = runParse parser initState
+modifyOffset :: ParseState -> Int64 -> ParseState
+-- modifyOffset (ParseState str ofs) n = ParseState str (ofs + n)
+modifyOffset initState newoffset = initState { offset = newoffset}
+addOffset :: ParseState -> Int64 -> ParseState
+-- modifyOffset (ParseState str ofs) n = ParseState str (ofs + n)
+-- addOffset initState addoffset = ParseState (string initState) ((offset initState) + addoffset)
+addOffset initState addoffset = initState {offset =  (offset initState) + addoffset}
+parseByte :: Parse Word8
+parseByte =
+    getState ==> \initState ->
+    case L.uncons (string initState) of
+      Nothing -> bail "no more input"
+      Just (byte, remainder) -> 
+          putState newState ==> \_ ->
+          identity byte
+        where newState = initState {string = remainder, offset = newOffset}
+              newOffset = offset initState + 1
+getState :: Parse ParseState
+getState = Parse (\s -> Right (s, s))
+putState :: ParseState -> Parse ()
+putState s = Parse (\_ -> Right ((), s))
+bail :: String -> Parse a
+bail err = Parse $ \s -> Left $ "byte offset" ++ show (offset s) ++ ": " ++ err
+(==>) :: Parse a -> (a -> Parse b) -> Parse b
+firstParser ==> secondParser = Parse chainedParser
+    where chainedParser initState = 
+             case runParse firstParser initState of
+                Left errMessage -> Left errMessage
+                Right (firstResult, newState) -> runParse (secondParser firstResult) newState
+
