777b33635bd3e975575b28ce873a924524e39848
unknown <Administrator@.(none)>
2010-09-13 23:35:59 +0900

    upd

--- RWH/Ch14/Do.hs
+++ RWH/Ch14/Do.hs
@@ -8,3 +8,19 @@ doNotation3 = do
 --                return x
 --   where xs = [0,1,2]
 --         ys = [9,10]
+robust :: [a] -> Maybe a
+robust xs = do 
+  (_:x:_) <- Just xs
+  return x
+robust' xs = 
+  Just xs >>= \(_:x:_) -> 
+  return x
+robust'' xs = 
+  let f (_:x:_) = return x
+      f _ = fail ""
+  in Just xs >>= f
+robust''' xs = 
+  Just xs >>= f
+  where f (_:x:_) = return x
+        f _ = fail ""
+wordCount = print . length . words =<< getContents
