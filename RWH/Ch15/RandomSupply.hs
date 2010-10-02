026975a9108613f6e28f7ccb7b62299a5c9795c0
ueki5 <ueki005@gmail.com>
2010-10-02 19:52:48 +0900

    upd

--- RWH/Ch15/RandomSupply.hs
+++ RWH/Ch15/RandomSupply.hs
@@ -9,6 +9,8 @@ randomsIO =
     getStdRandom $ \g ->
         let (a, b) = split g
         in (randoms a, b)
+randomsIO' g = let (a, b) = split g
+               in (randoms a, b)
 
 randomsIO_golfed :: Random a => IO [a]
 randomsIO_golfed = getStdRandom (first randoms . split) 
