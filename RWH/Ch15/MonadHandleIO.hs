dfa894240ea3b2b27d66b151049e7f6de6b9663e
ueki <ueki005@gmail.com>
2010-10-04 18:11:15 +0900

    upd

--- RWH/Ch15/MonadHandleIO.hs
+++ RWH/Ch15/MonadHandleIO.hs
@@ -14,7 +14,6 @@ instance MonadHandle System.IO.Handle IO where
     hClose = System.IO.hClose
     hGetContents = System.IO.hGetContents
     hPutStrLn = System.IO.hPutStrLn
-
 class (MonadHandle h m, MonadIO m) => MonadHandleIO h m | m -> h
 
 instance MonadHandleIO System.IO.Handle IO
--- RWH/Ch15/WriterIO.hs
+++ RWH/Ch15/WriterIO.hs
@@ -1,8 +1,11 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 module Ch15.WriterIO where
 
-import System.IO (IOMode(..))
+import System.IO --(IOMode(..))
 import Control.Monad.Writer
+import Ch15.MonadHandleIO
 import Ch15.MonadHandle
 import Ch15.SafeHello
 data Event = Open FilePath IOMode
@@ -14,3 +17,9 @@ newtype WriterIO a = W { runW :: Writer [Event] a}
     deriving (Monad, MonadWriter [Event])
 runWriterIO :: WriterIO a -> (a, [Event])
 runWriterIO = runWriter . runW
+    
+instance MonadHandle FilePath WriterIO where
+    openFile path mode = tell [Open path mode] >> return path
+    hPutStr h str = tell [Put h str]
+    hClose h = tell [Close h]
+    hGetContents h = tell [GetContents h] >> return ""
