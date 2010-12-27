6c5d21ccf79483675b46dd7f239de00244786eb5
ueki <ueki005@gmail.com>
2010-12-27 20:17:25 +0900

    Ch18

---
+++ RWH/Ch18/CountEntriesT.hs
@@ -0,0 +1,18 @@
+-- file: Ch18/CountEntriesT.hs
+module Ch18.CountEntriesT (listDirectory, countEntries) where
+
+import Ch18.CountEntries (listDirectory)
+import System.Directory (doesDirectoryExist)
+import System.FilePath ((</>))
+import Control.Monad (forM_, when)
+import Control.Monad.Trans (liftIO)
+import Control.Monad.Writer (WriterT, tell)
+
+countEntries :: FilePath -> WriterT [(FilePath, Int)] IO ()
+countEntries path = do
+  contents <- liftIO . listDirectory $ path
+  tell [(path, length contents)]
+  forM_ contents $ \name -> do
+    let newName = path </> name
+    isDir <- liftIO . doesDirectoryExist $ newName
+    when isDir $ countEntries newName
