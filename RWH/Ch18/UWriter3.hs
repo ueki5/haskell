eaf11a1acb1e8d931ebedc1f0b32bc0bcf7e5342
ueki5 <ueki005@gmail.com>
2011-01-18 04:35:43 +0900

    upd

--- RWH/Ch18/UWriter3.hs
+++ RWH/Ch18/UWriter3.hs
@@ -1,23 +1,22 @@
 module Ch18.UWriter3 where
 import Control.Monad
 
-runLog :: Log a -> (a, String)
-runLog = execLog
-record :: String -> Log ()
-record s = Log ((), s)
 data Log a = Log {execLog::(a, String)} deriving (Show)
 instance Monad Log where
-  return a = Log (a, "")
+  return a = Log (a, [])
   m >>= k = let (a, s) = execLog m
                 n = k a
                 (b, s') = execLog n
             in Log (b, s ++ "," ++ s')
+record :: String -> Log ()
+record s = Log ((), s)
 calc :: Int -> Log Int
 calc n = Log ( 2 * n, show n)
 go :: Int -> Log Int
-go n = return n >>= calc
-     >>= calc
-     >>= calc
+go n = return n 
+       >>= calc
+       >>= calc
+       >>= calc
 go2 n = do
   a <- return n 
   b <- calc a
