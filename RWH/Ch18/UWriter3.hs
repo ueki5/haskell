27037aff9892b56040d72b63f82a7023cfeadd71
ueki5 <ueki005@gmail.com>
2011-01-20 00:38:51 +0900

    u

--- RWH/Ch18/UWriter3.hs
+++ RWH/Ch18/UWriter3.hs
@@ -1,24 +1,32 @@
-module Ch18.UWriter3 
-       (
-         Log,
-         addn,
-         execLog
-       ) where
+module Ch18.UWriter3 (Log,execLog) where
 import Control.Monad
 
-data Log a = Log {execLog::(a, String)} deriving (Show)
+data Log a = LogD {execLog::(a, String)} deriving (Show)
 instance Monad Log where
-  return a = Log (a, [])
-  m >>= k = let (a, s) = execLog m
+  return a = LogD (a, [])
+  m >>= k = let (a, s ) = execLog m
                 n = k a
                 (b, s') = execLog n
-            in Log (b, s ++ "," ++ s')
+            in LogD (b, s ++ "," ++ s')
 calc :: Int -> Log Int
-calc n = Log ( 2 * n, show n)
-addn :: Int -> Int -> Log Int
-addn n m = Log (n + m, show m )
+calc n = LogD ( 2 * n, show n)
 go :: Int -> Log Int
 go n = return n 
-       >>= addn 1
-       >>= addn 2
-       >>= addn 3
+       >>= calc
+       >>= calc
+       >>= calc
+
+data Log1 a = LogD1 {execLog1::(a, [String])} deriving (Show)
+instance Monad Log1 where
+  return a = LogD1 (a, [])
+  m >>= k = let (a, s ) = execLog1 m
+                n = k a
+                (b, s') = execLog1 n
+            in LogD1 (b, s ++ s')
+calc1 :: Int -> Log1 Int
+calc1 n = LogD1 ( 2 * n, [show n])
+go1 :: Int -> Log1 Int
+go1 n = return n 
+       >>= calc1
+       >>= calc1
+       >>= calc1
