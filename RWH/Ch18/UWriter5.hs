096dbb476d0ad1e55d956ae4f60887b3f4211bbf
ueki5 <ueki005@gmail.com>
2011-01-21 03:46:10 +0900

    u

--- RWH/Ch18/UWriter5.hs
+++ RWH/Ch18/UWriter5.hs
@@ -1,17 +1,30 @@
--- uekic5 = forM [0,1,2,3] \x-> x**2
+module Ch18.Uwriter5 where
 import Control.Monad
+data Uist a = UistD a (Uist a) | Null deriving (Show)
+instance Monad Uist where
+  return a = UistD a Null
+  (>>=) Null k = Null
+  (>>=) (UistD a cs) k = (k a) `concate` (>>=) cs k
+    where 
+      concate :: Uist a -> Uist a -> Uist a
+      Null `concate` as = as
+      (UistD a as) `concate` as' = UistD a (as `concate` as')
 
-data Ueki a = UekiD {execUeki::(a, String)} deriving (Show)
-instance Monad Ueki where
-  return a = UekiD (a, [])
-  m >>= k = let (a, s) = execUeki m
-                n = k a
-                (b, s') = execUeki n
-            in UekiD (b, s' ++ s)
-a :: [Int]
-a = [0,1,2,3]
-ueki5 :: Int -> Ueki Int
-ueki5 cs = UekiD (cs, show cs)
--- myForM :: (Monad m) => [a] -> (a -> m b) -> m [b]
--- myForM [] k = 
-  
+ueki :: String -> Uist String
+ueki [] = Null
+ueki (c:cs) = UistD [c] $ UistD [c] $ ueki cs
+
+uekiG :: String -> Uist String
+uekiG s = ueki s 
+          >>= ueki
+-- data Ueki a = UekiD {execUeki::(a, String)} deriving (Show)
+-- instance Monad Ueki where
+--   return a = UekiD (a, [])
+--   m >>= k = let (a, s) = execUeki m
+--                 n = k a
+--                 (b, s') = execUeki n
+--             in UekiD (b, s' ++ s)
+-- a :: [Int]
+-- a = [0,1,2,3]
+-- uekiI2S :: Int -> Ueki Int
+-- uekiI2S cs = UekiD (cs, show cs)
