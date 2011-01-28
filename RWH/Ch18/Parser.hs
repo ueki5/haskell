5520f7a1276738dfbd885e47d1b877dd7d2d0d67
ueki <ueki005@gmail.com>
2011-01-28 20:46:33 +0900

    upd

--- RWH/Ch18/Parser.hs
+++ RWH/Ch18/Parser.hs
@@ -1,4 +1,15 @@
 module Ch18.Parser where
 import Control.Monad
 
--- Parser String -> [Int]
+-- data Parser a = ParserD {execParser :: String -> [(a, String)]}
+-- instance Monad Parser where
+--     return a = ParserD $ \s -> (a, [s])
+--     m >>= k = let ns = execParser m ""
+--                   case ns of
+--                     [] -> ParserD 
+
+data Parser a = ParserD {execParser :: String -> a}
+instance Monad Parser where
+    return a = ParserD $ \s -> a
+    m >>= k = f
+      where f s = k $ execParser m s
