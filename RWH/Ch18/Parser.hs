77d1712047ef81f787a686a0f3bd8f1e37d7afaf
ueki5 <ueki005@gmail.com>
2011-02-01 02:56:06 +0900

    u

--- RWH/Ch18/Parser.hs
+++ RWH/Ch18/Parser.hs
@@ -1,5 +1,6 @@
 module Ch18.Parser where
 import Control.Monad
+import Data.Char
 
 data Parser a = ParserD {execParser::String -> [(a, String)]}
 instance Monad Parser where
@@ -15,40 +16,84 @@ item :: Parser Char
 item = ParserD $ \inp -> case inp of
   [] -> []
   (c:cs) -> [(c, cs)]
+check :: Parser Char
+check = ParserD $ \inp -> case inp of
+  [] -> []
+  s@(c:cs) -> [(c, s)]
 parser :: Parser a -> String -> [(a, String)]
 parser = execParser
+(+++) :: Parser a -> Parser a -> Parser a
+p +++ q = ParserD $ \inp -> case parser p inp of
+  [] -> parser q inp
+  [(v, out)] -> [(v, out)]
 
-uekigo :: Parser (Char, Char, Char)
-uekigo = item >>= \x -> 
-         item >>= \y -> 
-         item >>= \z ->
-         return (x, y, z)
+sat :: (Char -> Bool) -> Parser Char
+sat p = do 
+  x <- item
+  if p x then return x
+         else failure
+digit :: Parser Char
+digit = sat isDigit
+lower :: Parser Char
+lower = sat isLower
+upper :: Parser Char
+upper = sat isUpper
+letter :: Parser Char
+letter = sat isAlpha
+alphanum :: Parser Char
+alphanum = sat isAlphaNum
+char :: Char -> Parser Char
+char c = sat (== c)
+string :: String -> Parser String
+string [] = return []
+string (c:cs) = do
+  x <- char c
+  xs <- string cs
+  return (x:xs)
+many :: Parser a -> Parser [a]
+many p = many1 p +++ return []
+many1 :: Parser a -> Parser [a]
+many1 p = do
+  v <- p
+  vs <- many p
+  return (v:vs)
+integer :: Parser String
+integer = many1 digit
+-- integer = do
+--   x <- digit
+--   xs <- integer
+--   return (x:xs)
 
-uekigo2 :: String -> [((Char, Char, Char), String)]
-uekigo2 s = parser uekigo s
-
-uekigo3 :: Parser Char
-uekigo3 = item >>= \x ->
-          return x
-
--- **************************************************************
--- data Parser a = ParserD {execParser :: String -> [(a, String)]}
--- instance Monad Parser where
---     return a = ParserD $ \s -> (a, [s])
---     m >>= k = let ns = execParser m ""
---                   case ns of
---                     [] -> ParserD 
-
--- **************************************************************
--- data Parser a = ParserD {execParser :: String -> a}
--- instance Monad Parser where
---     return a = ParserD $ \s -> a
---     -- m >>= k = f
---     --   where f s = k $ execParser m s
--- parser :: String -> [(Int, String)]
--- parser s = [(read s,s)]
--- int2par :: Int -> [(Int, String)]
--- int2par n = [(n, show n)]
--- compo :: String -> [(Int, String)]
--- compo s = let pairs = parser s
---           in  concat $ map (\x -> int2par $ fst x) pairs
+-- integer :: Parser String
+-- integer = check >>= \x ->
+--   if isDigit x 
+--     then 
+--       item >>= \x' ->
+--       integer >>= \cs ->
+--       return (x:cs)
+--     else 
+--       return []
+-- integer = item >>= \x->
+--             if isDigit x 
+--             then 
+--               item >>= \x'->
+--                 if isDigit x' 
+--                 then 
+--                   item >>= \x''->
+--                     if isDigit x'' 
+--                     then 
+--                       return (x:x':[x''])
+--                     else 
+--                       return (x:[x'])
+--                 else 
+--                   return [x]
+--             else 
+--               return []
+-- integer = check >>= \x->
+--             if isDigit x 
+--             then 
+--               item >>= \x' -> 
+--               integer >>= \s ->
+--               return (x:s)
+--             else 
+--               return []
