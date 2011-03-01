bca6871eee3ff08a9642c16c92946e3c364a9707
ueki <ueki@ueki-PC.(none)>
2011-03-02 00:39:23 +0900

    upd

--- Cflat/Parser/Parser.hs
+++ Cflat/Parser/Parser.hs
@@ -23,14 +23,6 @@ item = Parser $ \inp -> case inp of
 p +++ q = Parser $ \inp -> case parser p inp of
                       Nothing -> parser q inp
                       Just (v, out) -> Just (v, out)
--- 双方が成功した場合に採用
-(&&&) :: Parser a -> Parser a -> Parser a
-p &&& q = Parser $ \inp -> case parser p inp of
-                      Nothing -> Nothing
-                      Just (v, out) -> case parser q inp of
-                        Nothing -> Nothing
-                        Just (v', out') -> if out == out' then Just (v', out')
-                                                      else Nothing
 sat :: (Char -> Bool) -> Parser Char
 sat p = do 
   x <- item
@@ -71,60 +63,21 @@ token p = do
   cs <- p
   space
   return cs
--- 数値
-int :: Parser Type
-int = do
-  cs <- token $ many1 digit
-  return $ TpInt (read cs)
-fint :: Parser Formula
-fint = do
-  arg <- int
-  return (Tp arg)
--- 演算子
-plus :: Parser Operator
-plus = token (char '+') >> return Plus
-minus :: Parser Operator
-minus = token (char '-') >> return Minus
-mult :: Parser Operator
-mult = token (char '*') >> return Mult
-divide :: Parser Operator
-divide = token (char '/') >> return Div
-power :: Parser Operator
-power = token (char '^') >> return Power
-
--- 足算、引算と掛算、除算をグループ化
-opr1 :: Parser Operator
-opr1 = plus +++ minus
-opr2 :: Parser Operator
-opr2 = mult +++ divide
-opr3 :: Parser Operator
-opr3 = power
-opr :: Parser Operator
-opr = opr1 +++ opr2 +++ opr3
--- レベル０（優先度最低：足算、引算　左結合）
-form :: Parser Formula
-form = do
-  frm <- form1
-  exprl frm opr1 form1
--- レベル１（優先度１：掛算、除算　右結合にしてみました）
-form1 = do
-  frm <- form2
-  exprr frm opr2 form1
---レベル２（優先度２：べき乗　左結合）
-form2 = do
-  val <- fint
-  exprl val opr3 fint
---左結合
-exprl :: Formula -> Parser Operator -> Parser Formula -> Parser Formula
-exprl frm o p = do
-  op <- o
-  frm' <- p
-  exprl (Op op frm frm') o p
-  +++ return frm
---右結合
-exprr :: Formula -> Parser Operator -> Parser Formula -> Parser Formula
-exprr frm o p = do
-  op <- o
-  frm' <- p
-  return (Op op frm frm')
-  +++ return frm
+compilationUnit :: Parser CompilationUnit
+compilationUnit = do
+  imp_stmts <- importStmts
+  top_defs <- topDefs
+  return $ CompilationUnit (imp_stmts, top_defs)
+importStmts ::  Parser ImportStmts
+importStmts =  undefined
+importStmt :: Parser ImportStmt
+importStmt = do
+  imp <- string "import"
+  nms <- names
+name :: Parser String
+name = do
+  head <- letter
+  
+names :: Parser Names
+topDefs :: Parser TopDefs
+topDefs = undefined
--- Cflat/Type/Type.hs
+++ Cflat/Type/Type.hs
@@ -1,13 +1,15 @@
 module Cflat.Type.Type where
 
-data Type =  TpInt Int
+data CompilationUnit = CompilationUnit (ImportStmts, TopDefs)
               deriving (Eq, Ord, Show)
-data Operator = Plus
-             | Minus
-             | Mult
-             | Div
-             | Power
-              deriving (Eq,Show)
-data Formula = Tp Type
-                      | Op Operator Formula Formula
-              deriving (Eq,Show)
+data ImportStmts =  ImportStmt
+                    | Cons ImportStmt ImportStmts
+                 deriving (Eq, Ord, Show)
+data ImportStmt = Import Names
+                 deriving (Eq, Ord, Show)
+data Names = Name
+             | Dot Name Names
+              deriving (Eq, Ord, Show)
+type Name = String
+data TopDefs = TopDefs
+              deriving (Eq, Ord, Show)
