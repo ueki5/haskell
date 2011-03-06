bfa129d3ed543306657d62257ad7a742b596c118
ueki5 <ueki005@gmail.com>
2011-03-07 00:14:22 +0900

    block

--- Cflat/Parser/Parser.hs
+++ Cflat/Parser/Parser.hs
@@ -64,18 +64,14 @@ token p = do
   return cs
 data CompilationUnit = CompilationUnit ImportStmts TopDefs
                      deriving (Eq, Ord, Show)
-type ImportStmts =  [ImportStmt]
-data ImportStmt =  Import Names
-                    deriving (Eq, Ord, Show)
-type Names = [Name] 
-type Name = String
-type Dot = String
-type TopDefs = [TopDef]
 compilationUnit :: Parser CompilationUnit
 compilationUnit = do
   imp_stmts <- importStmts
   top_defs <- topDefs
   return $ CompilationUnit imp_stmts top_defs
+type ImportStmts =  [ImportStmt]
+data ImportStmt =  Import Names
+                    deriving (Eq, Ord, Show)
 importStmts ::  Parser ImportStmts
 importStmts =  many importStmt
 importStmt :: Parser ImportStmt
@@ -84,37 +80,26 @@ importStmt = do
   nms <- names
   semc <- token $ string ";"
   return $ Import nms
+type Names = [Name] 
+type Name = String
 names :: Parser Names
 names = do  
   nm <- name
-  do 
-    dot <- token $ string "."
-    nms <- names
-    return (nm:nms)
-    +++ return [nm]
+  nms <- many $ separator "." name
+  return (nm:nms)
 name :: Parser Name
 name = token $ do
   alf <- letter
   alfnums <- many alphanum
   return $ alf:alfnums
+type TopDefs = [TopDef]
+data TopDef = TopDef
+            | TopDefvars Defvars
+            | TopDefun Defun
+             deriving (Eq, Ord, Show)
 topDefs :: Parser [TopDef]
 topDefs = do
   many1 topDef
-data TopDef = TopDef
-            | TopDefvars Defvars
-             deriving (Eq, Ord, Show)
-type Defvars = [Defvar]
-data Defvar = Defvar Storage VarType Name Value
-             deriving (Eq, Ord, Show)
-data Storage = NoStorage
-                    | Static
-               deriving (Eq, Ord, Show)
-data VarType = IntType
-             | StrType
-               deriving (Eq, Ord, Show)
-data Value = NoValue
-           | Value String
-               deriving (Eq, Ord, Show)
 topDef :: Parser TopDef
 topDef = 
   defun
@@ -123,29 +108,92 @@ topDef =
   -- +++ defstruct
   -- +++ defunion
   -- +++ typedef
-defun = undefined
+data Defun = Defun  Storage VarType Name Params Block
+             deriving (Eq, Ord, Show)
+defun :: Parser TopDef
+defun = do
+  strg <- storage
+  tp <- vartyperef
+  nm <- name
+  prms <- parenthesis "(" params ")"
+  blk <- block
+  return (TopDefun  (Defun strg tp nm prms blk))
+data Params = Void
+            | FixedParam [Param]
+            | UnfixedParam [Param]
+              deriving (Eq, Ord, Show)
+params = 
+    do 
+      prm <- param
+      prms <- many (separator "," param)
+      do
+        unfixed <- separator "," (token $ string "...")
+        return (UnfixedParam (prm:prms))
+        +++ return (FixedParam (prm:prms))
+    +++
+    do
+      v <- void
+      return v
+void = token $ do
+  string "void"
+  return Void
+data Param = Param VarType Name
+             deriving (Eq, Ord, Show)
+param = do
+  tp <- vartype
+  nm <- name
+  return $ Param tp nm
+data Block = Block Defvarlist Stmts
+               deriving (Eq, Ord, Show)
+block = parenthesis 
+  "{" 
+  (do
+     lst <- defvarlist
+     ss <- stmts
+     return $ Block lst ss)
+  "}"
+type Defvarlist = [Defvars]
+defvarlist = many defvar
+defvar = do
+  tp <- vartype
+  valnm <- varname
+  valnms <- many (separator "," varname)
+  return (map (\(nm,  val) -> (Defvar NoStorage tp nm val)) (valnm:valnms))
+type Stmts = [Stmt]
+data Stmt = Stmt
+               deriving (Eq, Ord, Show)
+stmts = undefined
+parenthesis :: String -> Parser a -> String -> Parser a
+parenthesis l p r = do
+  string l
+  elm <- p
+  string r
+  return elm
+type Defvars = [Defvar]
+data Defvar = Defvar Storage VarType Name Value
+             deriving (Eq, Ord, Show)
 defvars :: Parser TopDef
-defvars = 
-  do
-    strg <- storage
-    tp <- vartype
-    valnm <- varname
-    valnms <- many (separator "," varname)
-    return (TopDefvars  (map (\(nm,  val) -> (Defvar strg tp nm val)) (valnm:valnms)))
-  +++ 
-  do  
-    tp <- vartype
-    valnm <- varname
-    valnms <- many (separator "," varname)
-    return (TopDefvars (map (\(nm,  val) -> (Defvar NoStorage tp nm val)) (valnm:valnms)))
+defvars = do
+  strg <- storage
+  tp <- vartype
+  valnm <- varname
+  valnms <- many (separator "," varname)
+  return (TopDefvars  (map (\(nm,  val) -> (Defvar strg tp nm val)) (valnm:valnms)))
 separator :: String -> Parser a -> Parser a
 separator spr p = do
   s <- token $ string spr
   a <- p
   return a
+data Storage = NoStorage
+                    | Static
+               deriving (Eq, Ord, Show)
 storage = do
     strg <- token $ string "static"
     return Static
+    +++ return NoStorage
+data VarType = IntType
+             | StrType
+               deriving (Eq, Ord, Show)
 vartype = token $ 
   do
     tp <- string "int"
@@ -154,14 +202,18 @@ vartype = token $
   do 
     tp <-string "string"
     return StrType
+vartyperef = vartype
 varname :: Parser (Name,  Value)
 varname = do
   nm <- name
   do
     eq <- token $ string "="
     val <- value
-    return $ (nm, val)
+    return (nm, val)
     +++ return (nm, NoValue)
+data Value = NoValue
+           | Value String
+               deriving (Eq, Ord, Show)
 value = token $ do
    val <- many1 alphanum
    return (Value val)
